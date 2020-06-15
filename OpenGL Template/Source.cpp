3b355505e886a4943765451c3bacfedb44ff3cca
Antonio Rodrigues Costa <metcot11@gmail.com>
2020-06-15 19:30:32 -0300

    multicolored fragments

--- OpenGL Template/Source.cpp
+++ OpenGL Template/Source.cpp
@@ -1,9 +1,10 @@
 #include <glew.h>
 #include <glfw3.h>
+
+#include"Textures/Texture Loader/stb_image.h"
 #include "Shader.h"
 
 #include <math.h>
-
 #include <iostream>
 
 
@@ -26,9 +27,15 @@ int main(void) {
 		return -1;
 		/* Rendering a triangule */
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, 
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  
+		 0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f
+	};
+
+	float texCoodenates[] = {
+		0.0f,  0.0f,
+		1.0f,  0.0f,
+		0.5f,  1.0f
 	};
 		/*Vertex buffer object*/
 	unsigned int VBO;/*Vetex Buffer object ID*/
@@ -40,22 +47,35 @@ int main(void) {
 	Shader shader("Shader/Fragment Shader.Shader","Shader/Vertex Shader.Shader");
 	shader.On();
 
+	
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+		/*Textres*/
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("Textures/conteiner.jpg", &width, &height, &nrChannels, 0);
+
+	unsigned int Texture;
+	glGenTextures(1, &Texture);
+
+	glBindTexture(GL_TEXTURE_2D, Texture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(data);
 	while (!glfwWindowShouldClose(Window)) {
 				/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-
-
+		
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 				/* Swap front and back buffers */
 		glfwSwapBuffers(Window);
-
-		
-		
 				/* Poll for and process events */
 		glfwPollEvents();
 	}
